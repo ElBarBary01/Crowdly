14000791fba7bbb161092396a86bc55fd7149be3
omar elbarbary <omar.m.elbarbary@gmail.com>
2026-08-23 13:23:15 +0300

    fix(prisma): update dotenv import and datasource URL handling; adjust schema generator settings

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,5 @@
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "../../generated/prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 export default prisma;

cef9960452c9c59b53565a16129d058a8fcf5834
Naira Ehab <nairaehabb8@gmail.com>
2026-08-25 15:36:06 +0300

    "SCRUM-27 Build the shared Footer component"

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import TopNavbar from "./components/ui/navigationComponent/topNavbar";
 import styles from "./layout.module.css";
 import "./globals.css";
+import Footer from "./components/ui/footerComponent";
 
 export const metadata: Metadata = {
   title: "Crowdly",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={styles.shell}>
         <TopNavbar />
         <main className={styles.main}>{children}</main>
+        <Footer />
       </body>
     </html>
   );

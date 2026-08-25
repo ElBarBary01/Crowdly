0119c04b7e2fb4401138e81e58cdfcb291920185
mohamed abdallah <mohamed234552@bue.edu.eg>
2026-08-25 11:48:06 +0300

    refactor/ organize feedback and navigation components into folders

--- client/app/components/ui/navigationComponent/topNavbar/index.tsx
+++ client/app/components/ui/navigationComponent/topNavbar/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import MobileNavbar from "./mobileNavbar";
+import MobileNavbar from "../mobileNavbar";
 import styles from "./topNavbar.module.css";
 
 const navigationItems = [
